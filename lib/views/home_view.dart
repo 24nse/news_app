5dcc895428035aaa6633f0ddf9518982b3c15aa8
nooh <nooh.saeed@gmail.com>
2025-06-07 09:15:51 +0300

    create category_view, article_model,news_service and some custom widgets

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/widgets/categories_list_view.dart';
+import 'package:news_app/widgets/news_list_view_builder.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -30,12 +32,13 @@ class HomeView extends StatelessWidget {
         child: CustomScrollView(
           physics: BouncingScrollPhysics(),
           slivers: [
-            SliverToBoxAdapter(),
+            SliverToBoxAdapter(child:CategoriesListView(),),
             SliverToBoxAdapter(
               child: SizedBox(
                 height: 32,
               ),
-            )
+            ),
+            NewsListViewBuilder(category: "general")
           ],
         ),
         ),
